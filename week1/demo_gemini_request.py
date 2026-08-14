254488a859eb187f081665f3e653225fba71e2c9
anormalguy96 <elmeddin.mohubbetov@gmail.com>
2026-08-14 17:44:45 +0400

    #

--- week1/demo_gemini_request.py
+++ week1/demo_gemini_request.py
@@ -1,31 +1,21 @@
 import os
-import sys
 from google import genai
 from dotenv import load_dotenv
 
-from google.genai import types
-
-if hasattr(sys.stdout, "reconfigure"):
-    sys.stdout.reconfigure(encoding="utf-8")
-
-env_path = os.path.join(os.path.dirname(__file__), ".env")
-load_dotenv(env_path)
+load_dotenv()
 
 api_key = os.getenv("GEMINI_API_KEY")
-model_name = os.getenv("MODEL_NAME", "gemini-2.5-flash")
 
-client = genai.Client(api_key=api_key)
+model = genai.Client(api_key=api_key)
 
-prompt = input("Prompt: ")
+while True:
+    prompt = input("Prompt: ")
+    if prompt == "exit":
+        break
 
-config = types.GenerateContentConfig(
-    automatic_function_calling=types.AutomaticFunctionCallingConfig(disable=True)
-)
+    response = model.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
 
-response = client.models.generate_content(
-    model=model_name,
-    contents=prompt,
-    config=config,
-)
-
-print(response.text)
+    print(response.text)
